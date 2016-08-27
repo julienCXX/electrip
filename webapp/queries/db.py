e1eca60c2d069e8cd5a45ad5b48e351235f45e07
julienCXX <software@chmodplusx.eu>
2016-08-27 12:46:57 +0200

    Better management of database sessions

--- webapp/queries/db.py
+++ webapp/queries/db.py
@@ -1,3 +1,4 @@
+from contextlib import contextmanager
 import json
 from urllib.parse import quote_plus as urlquote
 from sqlalchemy import create_engine
@@ -13,11 +14,27 @@ import app_config
 _engine = create_engine('postgresql://%s:%s@localhost/%s' %
                         (app_config.DB_USER, urlquote(app_config.DB_PASSWORD),
                          app_config.DB_NAME))
-_sess_maker = sessionmaker(bind=_engine)
+_Session = sessionmaker(bind=_engine)
+
+
+@contextmanager
+def _session_scope():
+    """Provide a transactional scope around a series of operations."""
+
+    session = _Session()
+    try:
+        yield session
+        session.commit()
+    except:
+        session.rollback()
+        raise
+    finally:
+        session.close()
 
 
 def get_station_types():
-    return _sess_maker().query(StationType).order_by(StationType.id)
+    with _session_scope() as session:
+        return session.query(StationType).order_by(StationType.id)
 
 
 def get_stations_json():
@@ -27,14 +44,16 @@ def get_stations_json():
 def get_euclidean_distance(p1, p2):
     "Returns the distance (crow-fly) between 2 points (in meters)"
 
-    res = _sess_maker().query(func.ST_Distance(p1.cast(Geography),
-                              p2.cast(Geography))).one()
+    with _session_scope() as session:
+        res = session.query(func.ST_Distance(p1.cast(Geography),
+                            p2.cast(Geography))).one()
     return res[0]
 
 
 def get_candidate_charge_points(start, finish, max_dist_from_finish,
                                 station_types, drive_range):
-    return _sess_maker().query(Station.position, Station.id).\
+    with _session_scope() as session:
+        return session.query(Station.position, Station.id).\
             filter(Station.type.in_(station_types)).\
             filter(func.ST_Distance(Station.position, start.cast(Geography)) <
                    drive_range).\
