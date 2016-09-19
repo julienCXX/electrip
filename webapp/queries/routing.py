f011876517ddd6ae436ce52ebe2566b216a6be5a
julienCXX <software@chmodplusx.eu>
2016-09-19 23:32:49 +0200

    Switched to OSRM API v5 for routing queries

--- webapp/queries/routing.py
+++ webapp/queries/routing.py
@@ -9,12 +9,12 @@ def get_effective_distance(p1, p2):
     "Returns the distance (using roads) between 2 points (in meters)"
 
     query = app_config.OSRM_URL + \
-        'viaroute?loc=%f,%f' % point_to_coords(p1) + \
-        '&loc=%f,%f&alt=false&geometry=false' % point_to_coords(p2)
+        'route/v1/driving/%f,%f;' % point_to_coords(p1, False) + \
+        '%f,%f?overview=false' % point_to_coords(p2, False)
     response = requests.get(url=query)
     r_json = response.json()
-    if 'route_summary' in r_json:
-        return r_json['route_summary']['total_distance']
+    if 'Ok' == r_json['code']:
+        return r_json['routes'][0]['distance']
 
     # No route available between points
     return sys.maxsize
@@ -24,19 +24,21 @@ def get_routing_instructions(start, finish, via_points, zoom_level):
     result = via_points
     if not result['route_found']:
         return result
-    query = app_config.OSRM_URL + 'viaroute?loc=%f,%f' % point_to_coords(start)
+    query = app_config.OSRM_URL + \
+        'route/v1/driving/%f,%f;' % point_to_coords(start, False)
     for point in via_points['stations']:
-        query = query + '&loc=%f,%f' % point_to_coords(point['position'])
+        query = query + '%f,%f;' % point_to_coords(point['position'], False)
     query = query + \
-        '&loc=%f,%f' % point_to_coords(finish) + \
-        '&z=%d&instructions=false&alt=false&compression=false' % (zoom_level)
+        '%f,%f' % point_to_coords(finish, False) + \
+        '?geometries=geojson&overview=full'
     response = requests.get(url=query)
     response = response.json()
-    if 'route_geometry' not in response:
+    if 'Ok' != response['code']:
         # No route available between points (TODO: temporary workaround)
         result['route_found'] = False
         return result
-    result['route_geometry'] = response['route_geometry']
-    # result['route_instructions'] = response['route_instructions']
-    result['route_summary'] = response['route_summary']
+    raw_route_geom = response['routes'][0]['geometry']['coordinates']
+    result['route_geometry'] = []
+    for coord in raw_route_geom:
+        result['route_geometry'].append((coord[1], coord[0]))
     return result
